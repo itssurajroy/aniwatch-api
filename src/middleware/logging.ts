c33a503c84e800bf2220933a6af8519e32a91207
itssurajroy <surajrai1204@gmail.com>
2025-09-30 21:58:14 +0530

    Update logging.ts

--- src/middleware/logging.ts
+++ src/middleware/logging.ts
@@ -1,9 +1,3 @@
-import type { MiddlewareHandler } from "hono";
-import { logger as honoLogger } from "hono/logger";
-import { log } from "../config/logger.js";
+import { logger } from "hono/logger";
 
-export const logging: MiddlewareHandler = honoLogger(
-    (msg: string, ...rest: string[]) => {
-        log.info(msg, ...rest);
-    }
-);
+export const logging = logger();
